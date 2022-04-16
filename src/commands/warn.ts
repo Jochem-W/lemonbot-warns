babd8f1e2f0a0d14b02f46b21e42026f29fcd2a7
Jochem <16637577+Jochem-W@users.noreply.github.com>
2022-04-16 21:44:11 +0200

    Add option to notify member

--- src/commands/warn.ts
+++ src/commands/warn.ts
@@ -2,7 +2,7 @@
  * @description Slash command which warns a user.
  */
 import CommandWrapper from "../types/commandWrapper"
-import {CommandInteraction, Permissions} from "discord.js"
+import {CommandInteraction, DiscordAPIError, GuildMember, Permissions} from "discord.js"
 import Embed from "../utilities/embed";
 import Database from "../utilities/database";
 
@@ -32,6 +32,10 @@ export default class WarnCommand extends CommandWrapper {
                     ["4: Ban/Blacklist", "4: Ban/Blacklist"]
                 ])
                 .setRequired(true))
+            .addBooleanOption(option => option
+                .setName("notify")
+                .setDescription("Whether or not to notify the user of the warning.")
+                .setRequired(true))
             .toJSON()
     }
 
@@ -46,21 +50,48 @@ export default class WarnCommand extends CommandWrapper {
             return
         }
 
+        if (!interaction.member) {
+            await interaction.editReply({
+                embeds: [
+                    Embed.make("Unknown member", "The user you specified is not a member of this server.")
+                ]
+            })
+
+            return
+        }
+
         const user = interaction.options.getUser("user", true)
         const reason = interaction.options.getString("reason", true)
         const penalty = interaction.options.getString("penalty", true)
-
-        let member
-        try {
-            member = await interaction.guild?.members.fetch(user)
-        } catch (e) {}
+        const member = interaction.member as GuildMember
 
         await Database.watchlistUpdate(user, reason, penalty, member)
 
-        const embed = Embed.make(`Warned ${user.tag}`, user.displayAvatarURL({
+        const embed = Embed.make(`Warned ${user.tag} in ${member.guild.name}`, user.displayAvatarURL({
             dynamic: true,
             size: 4096
-        }), `Reason: ${reason}`)
+        }), `Reason: ${reason}`).addField("Penalty level", penalty)
+
+        if (interaction.options.getBoolean("notify", true)) {
+            try {
+                await member.send({
+                    embeds: [
+                        Embed.make("Warning", undefined, `You have been warned in ${member.guild.name}.`)
+                            .addField("Reason", reason)
+                            .setColor("#ff0000"),
+                    ]
+                })
+
+                embed.addField("Notification", "Successfully notified the user.")
+            } catch (e) {
+                if (e instanceof DiscordAPIError) {
+                    embed.addField("Notification", `Error: ${e.message}`)
+                } else {
+                    embed.addField("Notification", "An unknown error occurred while notifying the user.")
+                }
+            }
+        }
+
         await interaction.editReply({embeds: [embed]})
     }
 }
