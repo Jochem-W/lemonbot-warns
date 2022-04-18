477b46b1107534c5be6328b72c06ef075d71e795
Jochem <16637577+Jochem-W@users.noreply.github.com>
2022-04-18 10:49:32 +0200

    Update config

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,7 @@
 export const Config = {
     guildIds: [
-        "854129731816914954"
-    ], authorIcon: "https://upload.wikimedia.org/wikipedia/commons/4/45/Notion_app_logo.png",
-    databaseId: "e40b49e15c64401a8209918452ebcb9c",
+        "952575508548292608"
+    ], databaseId: "e40b49e15c64401a8209918452ebcb9c",
     warnIcon: "https://cdn.discordapp.com/emojis/955886482835533855.png",
     successIcon: "https://cdn.discordapp.com/emojis/952623108181864590.png",
     failIcon: "https://cdn.discordapp.com/emojis/952623117623230485.png"
