222c86c4371cf231b3f8f2a3694eb5f3f8ca4e87
Jochem <16637577+Jochem-W@users.noreply.github.com>
2022-04-26 13:59:01 +0200

    Enable noImplicitOverride

--- src/wrappers/handlerWrapper.ts
+++ src/wrappers/handlerWrapper.ts
@@ -1,13 +1,13 @@
 /**
  * Handler for events on the Discord API, via discord.js
  */
-export default class HandlerWrapper {
+export default abstract class HandlerWrapper {
     public readonly eventName: string
 
     /**
      * @param eventName The event name to listen on. (see Events @ https://discord.js.org/#/docs/discord.js/stable/class/Client)
      */
-    constructor(eventName: string) {
+    protected constructor(eventName: string) {
         this.eventName = eventName
     }
 
@@ -15,6 +15,5 @@ export default class HandlerWrapper {
      * Custom code handler that processes the event received.
      * @param args The passed arguments from the event.
      */
-    async handle(...args: any[]): Promise<void> {
-    }
+    abstract handle(...args: any[]): Promise<void>
 }
