0277b148479f2fca8cfda4ae84afd36d432da746
Jochem <16637577+Jochem-W@users.noreply.github.com>
2022-04-29 13:22:30 +0200

    Remove unused config values

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,6 @@ export const Config = {
     successIcon: "CHANGE_ME", // Icon that is used on success
     failIcon: "CHANGE_ME", // Icon that is used on fail
     guildId: "CHANGE_ME", // Guild to enable the bot on
-    allowedRoleIds: ["ADD_ROLE_IDS_HERE"], // Roles that can use the bot
-    allowedUserIds: ["ADD_USER_IDS_HERE"], // Users that can use the bot
     privateChannels: ["ADD_CHANNEL_IDS_HERE"], // Channels that show non-ephemeral replies
     cacheTtl: 0, // Time in milliseconds to cache database data
 }
