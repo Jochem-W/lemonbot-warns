a138bec401b2ff6c3242a2e734d9f258c84bfedb
Jochem <16637577+Jochem-W@users.noreply.github.com>
2022-06-15 14:44:06 +0200

    Fix comments

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,25 @@
 export const Config = {
-    warnIcon: "CHANGE_ME", // Icon that is used when someone is warned
-    successIcon: "CHANGE_ME", // Icon that is used on success
-    failIcon: "CHANGE_ME", // Icon that is used on fail
-    guildId: "CHANGE_ME", // Guild to enable the bot on
-    privateChannels: ["ADD_CHANNEL_IDS_HERE"], // Channels that show non-ephemeral replies
-    cacheTtl: 0, // Time in milliseconds to cache database data
-    warnCategory: "CHANGE_ME", // The category under which to create a channel to warn someone
-    reportChannel: "CHANGE_ME", // The channel to send message reports to
+    // Icon that is used when someone is warned
+    warnIcon: "CHANGE_ME",
+
+    // Icon that is used on success
+    successIcon: "CHANGE_ME",
+
+    // Icon that is used on fail
+    failIcon: "CHANGE_ME",
+
+    // Guild to enable the bot on
+    guildId: "CHANGE_ME",
+
+    // Channels that show non-ephemeral replies
+    privateChannels: ["ADD_CHANNEL_IDS_HERE"],
+
+    // Time in milliseconds to cache database data
+    cacheTtl: 0,
+
+    // The category under which to create a channel to warn someone
+    warnCategory: "CHANGE_ME",
+
+    // The channel to send message reports to
+    reportChannel: "CHANGE_ME",
 }
