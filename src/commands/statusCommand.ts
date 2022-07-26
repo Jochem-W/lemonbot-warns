8bafd2b12d604f82255405cbc09ae140135df410
Jochem <16637577+Jochem-W@users.noreply.github.com>
2022-07-26 14:21:35 +0200

    Update status command formatting

--- src/commands/statusCommand.ts
+++ src/commands/statusCommand.ts
@@ -1,5 +1,5 @@
 import {ChatInputCommand} from "../models/chatInputCommand"
-import {ChatInputCommandInteraction, PermissionFlagsBits, WebhookEditMessageOptions} from "discord.js"
+import {ChatInputCommandInteraction, inlineCode, PermissionFlagsBits, WebhookEditMessageOptions} from "discord.js"
 import {Duration} from "luxon"
 import {ResponseBuilder} from "../utilities/responseBuilder"
 import {Variables} from "../variables"
@@ -14,25 +14,24 @@ export class StatusCommand extends ChatInputCommand {
     }
 
     public static buildResponse(options: ResponseOptions): WebhookEditMessageOptions {
-        const uptime = Duration.fromMillis(process.uptime() * 1000)
+        const uptime = Duration.fromObject(Object.fromEntries(Object.entries(Duration.fromMillis(process.uptime() *
+            1000)
             .shiftTo("days", "hours", "minutes", "seconds")
             .normalize()
+            .toObject())
+            .filter(([, value]) => value !== 0)))
 
         return {
             embeds: [ResponseBuilder.makeEmbed("Status")
                 .addFields({
                     name: "Ping",
-                    value: `${options.ping}ms`,
+                    value: inlineCode(`${options.ping}ms`),
                 }, {
                     name: "Uptime",
-                    value: `Up for \`${uptime.toHuman({
-                        listStyle: "long",
-                        notation: "compact",
-                        unitDisplay: "short",
-                    })}\``,
+                    value: inlineCode(uptime.toHuman({maximumFractionDigits: 0})),
                 }, {
                     name: "Version",
-                    value: Variables.commitHash ?? "unknown",
+                    value: inlineCode(Variables.commitHash ?? "unknown"),
                 })],
         }
     }
