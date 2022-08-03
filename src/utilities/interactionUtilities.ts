5388943c172ea301074889761a0bfdafa35fee2b
Jochem <16637577+Jochem-W@users.noreply.github.com>
2022-08-03 16:33:57 +0200

    Add isInPrivateChannel and isFromOwner

--- src/utilities/interactionUtilities.ts
+++ src/utilities/interactionUtilities.ts
@@ -1,4 +1,5 @@
-import {DiscordAPIError, Guild, GuildMember, Interaction, RESTJSONErrorCodes, UserResolvable} from "discord.js"
+import {DiscordAPIError, Guild, GuildMember, Interaction, RESTJSONErrorCodes, Team, UserResolvable} from "discord.js"
+import {DefaultConfig} from "../models/config"
 
 export async function fetchGuild(interaction: Interaction): Promise<Guild | null> {
     if (!interaction.inGuild()) {
@@ -29,4 +30,36 @@ export async function fetchMember(interaction: Interaction,
 
         throw e
     }
+}
+
+export async function isFromOwner(interaction: Interaction): Promise<boolean> {
+    if (!interaction.client.application) {
+        throw new Error()
+    }
+
+    let application = interaction.client.application
+    if (!application.owner) {
+        application = await application.fetch()
+        if (!application.owner) {
+            throw new Error()
+        }
+    }
+
+    if (application.owner instanceof Team) {
+        return application.owner.members.has(interaction.user.id)
+    }
+
+    return application.owner === interaction.user
+}
+
+export function isInPrivateChannel(interaction: Interaction) {
+    if (!interaction.inGuild()) {
+        return false
+    }
+
+    if (!interaction.channelId) {
+        return false
+    }
+
+    return DefaultConfig.guild.privateChannels.includes(interaction.channelId)
 }
