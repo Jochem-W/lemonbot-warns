771a0ce9e24ea027eca09c75cee78f44ec425e4e
Jochem <16637577+Jochem-W@users.noreply.github.com>
2022-10-13 20:06:56 +0200

    Rework message history downloading

--- src/commands/messageHistoryCommand.ts
+++ src/commands/messageHistoryCommand.ts
@@ -1,5 +1,5 @@
 import {ChatInputCommand} from "../models/chatInputCommand"
-import {ChatInputCommandInteraction, PermissionFlagsBits} from "discord.js"
+import {ChatInputCommandInteraction, inlineCode, PermissionFlagsBits} from "discord.js"
 import {Variables} from "../variables"
 import {isFromOwner} from "../utilities/interactionUtilities"
 import {OwnerOnlyError} from "../errors"
@@ -23,37 +23,68 @@ export class MessageHistoryCommand extends ChatInputCommand {
             throw new OwnerOnlyError()
         }
 
+        const userMessageKeys: string[] = []
         const user = interaction.options.getUser("user", true)
-        const keys: string[] = []
 
-        for await (const userMessageObject of search(Variables.s3ArchiveBucketName, `users/${user.id}/`)) {
-            if (!userMessageObject.Key) {
-                continue
-            }
-
-            const messageId = userMessageObject.Key.split("/").pop()
-            for await (const messageObject of search(Variables.s3ArchiveBucketName, `messages/${messageId}/`)) {
-                if (!messageObject.Key) {
+        let max = 0
+        let completed = false
+        const searchUserMessages = (async () => {
+            for await (const userMessageObject of search(Variables.s3ArchiveBucketName, `users/${user.id}/`)) {
+                if (!userMessageObject.Key) {
                     continue
                 }
 
-                keys.push(messageObject.Key)
+                userMessageKeys.push(userMessageObject.Key)
+                max++
+            }
+
+            completed = true
+        })()
+
+        const archive = archiver("tar", {gzip: true})
+        let current = 0
+        let lastEdited = 0
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+        while (!completed || userMessageKeys.length > 0) {
+            if (Date.now() - lastEdited > 2500) {
+                lastEdited = await this.reportProgress(current, max, interaction)
+            }
+
+            if (userMessageKeys.length > 0) {
+                current++
+            }
+
+            for await (const messageObject of
+                search(Variables.s3ArchiveBucketName, `messages/${userMessageKeys.shift()?.split("/").pop()}/`)) {
+                if (messageObject.Key) {
+                    // TODO: download in parallel?
+                    const stream = await download(Variables.s3ArchiveBucketName, messageObject.Key)
+                    archive.append(stream as Readable, {name: messageObject.Key})
+                }
             }
         }
 
-        const archive = archiver("tar", {gzip: true})
-        for (const key of keys) {
-            const stream = await download(Variables.s3ArchiveBucketName, key)
-            archive.append(stream as Readable, {name: key})
-        }
+        await searchUserMessages
 
         await archive.finalize()
-
         await interaction.editReply({
+            content: null,
             files: [{
                 attachment: archive,
                 name: `${user.id}.tar.gz`,
             }],
         })
+    }
+
+    private async reportProgress(current: number, max: number, interaction: ChatInputCommandInteraction) {
+        const barLength = 20
+        const progress = max !== 0 ? Math.min(current / max, 1) : 0
+        let bar = "=".repeat(Math.floor(progress * barLength))
+        bar += " ".repeat(barLength - bar.length)
+        const message = await interaction.editReply({
+            content: inlineCode(`[${bar}] ${Math.floor(progress * 100)}% (${current}/${max})`),
+        })
+
+        return message.editedTimestamp ?? Date.now()
     }
 }
