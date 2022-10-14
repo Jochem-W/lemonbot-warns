67846cf781966ff0805f9ab52b0311cb0ccb19a3
Jochem <16637577+Jochem-W@users.noreply.github.com>
2022-10-14 15:57:45 +0200

    Use async-await-queue to download in parallel

--- src/commands/messageHistoryCommand.ts
+++ src/commands/messageHistoryCommand.ts
@@ -4,8 +4,9 @@ import {Variables} from "../variables"
 import {isFromOwner} from "../utilities/interactionUtilities"
 import {OwnerOnlyError} from "../errors"
 import {download, search} from "../utilities/s3Utilities"
-import archiver from "archiver"
+import archiver, {Archiver} from "archiver"
 import {Readable} from "stream"
+import Queue from "async-await-queue"
 
 export class MessageHistoryCommand extends ChatInputCommand {
     public constructor() {
@@ -25,35 +26,38 @@ export class MessageHistoryCommand extends ChatInputCommand {
 
         await interaction.editReply(inlineCode("Searching"))
 
+        const queue = new Queue(3, 100)
+        const downloads: Promise<void>[] = []
+        let editOnProgress: NodeJS.Timeout | undefined
+        const report = () => {
+            this.reportProgress(downloads.length - queue.stat().waiting, downloads.length, interaction).catch(e => {
+                console.error(e)
+            })
+
+            editOnProgress = setTimeout(report, 1000)
+        }
+        report()
+
         const user = interaction.options.getUser("user", true)
-        const userMessageKeys: string[] = []
+        const archive = archiver("zip", {zlib: {level: 9}})
         for await (const userMessageObject of search(Variables.s3ArchiveBucketName, `users/${user.id}/`)) {
             if (!userMessageObject.Key) {
                 continue
             }
 
-            userMessageKeys.push(userMessageObject.Key)
-        }
-
-        const archive = archiver("tar", {gzip: true})
-        let current = 0
-        let lastEdited = 0
-        for (const key of userMessageKeys) {
-            if (Date.now() - lastEdited > 1000) {
-                lastEdited = await this.reportProgress(current, userMessageKeys.length, interaction)
-            }
-
-            for await (const messageObject of
-                search(Variables.s3ArchiveBucketName, `messages/${key.split("/").pop()}/`)) {
-                if (messageObject.Key) {
-                    const stream = await download(Variables.s3ArchiveBucketName, messageObject.Key)
-                    archive.append(stream as Readable, {name: messageObject.Key})
+            downloads.push((async (key) => {
+                const me = Symbol()
+                await queue.wait(me, 0)
+                try {
+                    await this.downloadMessage(archive, key)
+                } finally {
+                    queue.end(me)
                 }
-            }
-
-            current++
+            })(userMessageObject.Key))
         }
 
+        await Promise.allSettled(downloads)
+        clearTimeout(editOnProgress)
         await archive.finalize()
         await interaction.editReply({
             content: null,
@@ -64,15 +68,24 @@ export class MessageHistoryCommand extends ChatInputCommand {
         })
     }
 
+    private async downloadMessage(archive: Archiver, key: string) {
+        for await (const messageObject of search(Variables.s3ArchiveBucketName, `messages/${key.split("/").pop()}/`)) {
+            if (!messageObject.Key) {
+                continue
+            }
+
+            const stream = await download(Variables.s3ArchiveBucketName, key)
+            archive.append(stream as Readable, {name: key})
+        }
+    }
+
     private async reportProgress(current: number, max: number, interaction: ChatInputCommandInteraction) {
         const barLength = 20
         const progress = max !== 0 ? Math.min(current / max, 1) : 0
         let bar = "=".repeat(Math.floor(progress * barLength))
         bar += " ".repeat(barLength - bar.length)
-        const message = await interaction.editReply({
+        await interaction.editReply({
             content: inlineCode(`[${bar}] ${Math.floor(progress * 100)}% (${current}/${max})`),
         })
-
-        return message.editedTimestamp ?? Date.now()
     }
 }
