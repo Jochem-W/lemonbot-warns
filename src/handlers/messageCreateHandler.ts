591d3735db72a2e16709c1337eb82b563bd8e89d
Jochem <16637577+Jochem-W@users.noreply.github.com>
2022-10-16 21:47:21 +0200

    Upload in parallel

--- src/handlers/messageCreateHandler.ts
+++ src/handlers/messageCreateHandler.ts
@@ -2,6 +2,7 @@ import {Handler} from "../interfaces/handler"
 import {Message} from "discord.js"
 import {Variables} from "../variables"
 import {upload} from "../utilities/s3Utilities"
+import Queue from "async-await-queue"
 
 export class MessageCreateHandler implements Handler<"messageCreate"> {
     public readonly event = "messageCreate"
@@ -12,27 +13,62 @@ export class MessageCreateHandler implements Handler<"messageCreate"> {
             return
         }
 
-        await upload(Variables.s3ArchiveBucketName,
-            `messages/${message.id}/message.json`,
-            JSON.stringify(message.toJSON(), null, 4),
-            "application/json")
-
-        await upload(Variables.s3ArchiveBucketName,
-            `users/${message.author.id}/messages/${message.id}`,
-            "",
-            "text/plain")
-
-        await upload(Variables.s3ArchiveBucketName,
-            `channels/${message.channel.id}/messages/${message.id}`,
-            "",
-            "text/plain")
+        const queue = new Queue(3, 100)
+        const uploads: Promise<void>[] = [
+            (async () => {
+                const me = Symbol()
+                await queue.wait(me, 0)
+                try {
+                    await upload(Variables.s3ArchiveBucketName,
+                        `messages/${message.id}/message.json`,
+                        JSON.stringify(message.toJSON(), null, 4),
+                        "application/json")
+                } finally {
+                    queue.end(me)
+                }
+            })(),
+            (async () => {
+                const me = Symbol()
+                await queue.wait(me, 0)
+                try {
+                    await upload(Variables.s3ArchiveBucketName,
+                        `users/${message.author.id}/messages/${message.id}`,
+                        "",
+                        "text/plain")
+                } finally {
+                    queue.end(me)
+                }
+            })(),
+            (async () => {
+                const me = Symbol()
+                await queue.wait(me, 0)
+                try {
+                    await upload(Variables.s3ArchiveBucketName,
+                        `channels/${message.channel.id}/messages/${message.id}`,
+                        "",
+                        "text/plain")
+                } finally {
+                    queue.end(me)
+                }
+            })(),
+        ]
 
         for (const [, attachment] of message.attachments) {
-            const response = await fetch(attachment.url)
-            await upload(Variables.s3ArchiveBucketName,
-                `messages/${message.id}/attachments/${attachment.id}/${attachment.name}`,
-                response.body ?? undefined,
-                attachment.contentType ?? undefined)
+            uploads.push((async () => {
+                const me = Symbol()
+                await queue.wait(me, 0)
+                try {
+                    const response = await fetch(attachment.url)
+                    await upload(Variables.s3ArchiveBucketName,
+                        `messages/${message.id}/attachments/${attachment.id}/${attachment.name}`,
+                        response.body ?? undefined,
+                        attachment.contentType ?? undefined)
+                } finally {
+                    queue.end(me)
+                }
+            })())
         }
+
+        await Promise.allSettled(uploads)
     }
 }
