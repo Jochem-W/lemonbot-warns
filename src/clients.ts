9d4635faaf4d81a9859911b263263b7215ac3d90
Jochem <16637577+Jochem-W@users.noreply.github.com>
2022-11-02 16:29:31 +0100

    Try to fix S3 client issues

--- src/clients.ts
+++ src/clients.ts
@@ -1,6 +1,8 @@
 import {Variables} from "./variables"
 import {PrismaClient} from "@prisma/client"
 import {S3Client} from "@aws-sdk/client-s3"
+import {NodeHttpHandler} from "@aws-sdk/node-http-handler"
+import {Agent} from "https"
 
 export const Prisma = new PrismaClient()
 export const S3 = new S3Client({
@@ -10,4 +12,12 @@ export const S3 = new S3Client({
         accessKeyId: Variables.s3AccessKeyId,
         secretAccessKey: Variables.s3SecretAccessKey,
     },
+    requestHandler: new NodeHttpHandler({
+        connectionTimeout: 5000,
+        socketTimeout: 10000,
+        httpsAgent: new Agent({
+            keepAlive: true,
+            maxCachedSessions: 1000,
+        }),
+    }),
 })
