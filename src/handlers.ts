f86e5dd0cf6484da3cdf529f7cedc8b9a0ab3aa4
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2022-11-23 00:19:57 +0100

    Disable logging

--- src/handlers.ts
+++ src/handlers.ts
@@ -4,18 +4,18 @@ import {InteractionHandler} from "./handlers/interactionHandler"
 import type {Handler} from "./interfaces/handler"
 import {CommandHandler} from "./handlers/commandHandler"
 import {MemberRemoveHandler} from "./handlers/memberRemoveHandler"
-import {MessageCreateHandler} from "./handlers/messageCreateHandler"
-import {MessageDeleteBulkHandler} from "./handlers/messageDeleteBulkHandler"
-import {MessageDeleteHandler} from "./handlers/messageDeleteHandler"
-import {MessageUpdateHandler} from "./handlers/messageUpdateHandler"
+// import {MessageCreateHandler} from "./handlers/messageCreateHandler"
+// import {MessageDeleteBulkHandler} from "./handlers/messageDeleteBulkHandler"
+// import {MessageDeleteHandler} from "./handlers/messageDeleteHandler"
+// import {MessageUpdateHandler} from "./handlers/messageUpdateHandler"
 
 export const Handlers: Handler<keyof ClientEvents>[] = [
     new CommandHandler(),
     new InteractionHandler(),
     new ReadyHandler(),
     new MemberRemoveHandler(),
-    new MessageCreateHandler(),
-    new MessageDeleteBulkHandler(),
-    new MessageDeleteHandler(),
-    new MessageUpdateHandler(),
+    // new MessageCreateHandler(),
+    // new MessageDeleteBulkHandler(),
+    // new MessageDeleteHandler(),
+    // new MessageUpdateHandler(),
 ]
