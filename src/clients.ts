3cca8ec443c37d198134645d8948bf6990c879aa
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2022-12-11 11:36:21 +0100

    Remove @aws-sdk/node-http-handler

--- src/clients.ts
+++ src/clients.ts
@@ -1,8 +1,6 @@
 import {Variables} from "./variables"
 import {PrismaClient} from "@prisma/client"
 import {S3Client} from "@aws-sdk/client-s3"
-import {NodeHttpHandler} from "@aws-sdk/node-http-handler"
-import {Agent} from "https"
 
 export const Prisma = new PrismaClient()
 export const S3 = new S3Client({
@@ -12,12 +10,4 @@ export const S3 = new S3Client({
         accessKeyId: Variables.s3AccessKeyId,
         secretAccessKey: Variables.s3SecretAccessKey,
     },
-    requestHandler: new NodeHttpHandler({
-        connectionTimeout: 5000,
-        socketTimeout: 10000,
-        httpsAgent: new Agent({
-            keepAlive: true,
-            maxCachedSessions: 1000,
-        }),
-    }),
 })
